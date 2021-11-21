eea774a0bc7bfd3288635e60328d73f2ba21aebc
daqw <90310791+daqw@users.noreply.github.com>
2021-11-21 12:55:59 +0530

    Q2

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,19 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-public class FibonacciSeries
+public class MonthExample
 {
     static void Main(string[] args)
     {
-        int n = 0, n1 = 1, n2, number;
-        Console.WriteLine("Enter the number: ");
-        number= Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(n + "\n" + n1 + "");
-        for(int i = 0; i < number; i++)
+        int month;
+        char ch;
+        do
         {
-            n2 = n + n1;
-            Console.WriteLine(n2 + "");
-            n = n1;
-            n1 = n2;
+            Console.WriteLine("Enter month");
+            month = Convert.ToInt32(Console.ReadLine());
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("31 days in momth");
+                    break;
+
+                case 2:
+                    Console.WriteLine("29 days in month");
+                    break;
+
+                case 3:
+                    Console.WriteLine("31 days in momth");
+                    break;
+
+                case 4:
+                    Console.WriteLine("30 days in momth");
+                    break;
+
+                case 5:
+                    Console.WriteLine("31 days in momth");
+                    break;
+
+                case 6:
+                    Console.WriteLine("30 days in momth");
+                    break;
+
+                case 7:
+                    Console.WriteLine("31 days in momth");
+                    break;
+
+                case 8:
+                    Console.WriteLine("31 days in momth");
+                    break;
+
+                case 9:
+                    Console.WriteLine("30 days in momth");
+                    break;
+
+                case 10:
+                    Console.WriteLine("31 days in momth");
+                    break;
+
+                case 11:
+                    Console.WriteLine("30 days in momth");
+                    break;
+
+                case 12:
+                    Console.WriteLine("31 days in momth");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Month");
+                    break;
+            }
+            Console.WriteLine("Enter Y or y to continue");
+            ch = Convert.ToChar(Console.ReadLine());
         }
+        while (ch == 'Y' || ch == 'y');
     }
 }
+
+
