1ad94a62a5f09432f1ea41c311cc3b662195766e
daqw <90310791+daqw@users.noreply.github.com>
2021-11-21 13:16:38 +0530

    Add files via upload

---
+++ Assignment 2/Q6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalAbstract
+{
+
+    abstract class Animal
+    {
+        public abstract void FoodHabits();
+    }
+
+    class Cornivorous : Animal
+    {
+        public override void FoodHabits()
+        {
+            Console.WriteLine("Animals eat only meet");
+        }
+    }
+    class Herbivorous : Animal
+    {
+        public override void FoodHabits()
+        {
+            Console.WriteLine("Animals eat only plants");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Herbivorous h=new Herbivorous();
+            h.FoodHabits();
+
+            Cornivorous c= new Cornivorous();
+            c.FoodHabits();
+
+            Console.ReadKey();
+        }
+    }
+}
