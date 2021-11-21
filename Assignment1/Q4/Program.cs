b69936ce1f6547622bf6cf67c8219cba61ea41ce
daqw <90310791+daqw@users.noreply.github.com>
2021-11-21 13:06:17 +0530

    Add files via upload

---
+++ Assignment1/Q4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeProperties
+{
+    class Employee
+    {
+        int empid;
+        string name, desg;
+        double salary;
+
+        public int EmployeeID
+        {
+            get { return empid; }
+            set { empid = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Desg
+        {
+            get { return desg; }
+            set { desg = value; }
+        }
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            emp.EmployeeID = 100;
+            emp.Name = "Harshal";
+            emp.Desg = "Traine";
+            emp.Salary = 40000;
+            Console.WriteLine("Employee Details: {0},{1},{2},{3}", emp.EmployeeID, emp.Name, emp.Desg, emp.Salary);
+            Console.ReadKey();
+        }
+    }
+}
