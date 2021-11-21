0887cb273637240e6f445e36f48159cdec39f43c
daqw <90310791+daqw@users.noreply.github.com>
2021-11-21 13:12:39 +0530

    Add files via upload

---
+++ Assignment 2/Q2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerationDays
+{
+     class Program
+    {
+        enum Days {Sunday, Monday, Tuesday, Wednesday, Friday, Saturday}
+        static void Main(string[] args)
+        {
+            int firstday = (int)Days.Tuesday;
+            Console.WriteLine(firstday);
+            Console.ReadKey();
+        }
+    }
+}
