51c6e2a9dee2ba868ff8b265d0191d95658922fa
daqw <90310791+daqw@users.noreply.github.com>
2021-11-21 13:18:45 +0530

    Add files via upload

---
+++ Assignment 3/Q2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedExample
+{
+    class Bird
+    {
+
+    }
+    sealed class Test : Bird { }
+    class Example: Test { }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
