0671159bebbcbb5ecfd56d058790e05aae3bfe19
daqw <90310791+daqw@users.noreply.github.com>
2021-11-24 23:22:17 +0530

    Add files via upload

---
+++ Assignment4/Q6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemperatureCalculatorLibrary;
+
+namespace TemperatureLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureCalculatorLibrary.TemperatureClass temp1 = new TemperatureCalculatorLibrary.TemperatureClass();
+            Console.WriteLine("Celsiuis Temperature " + temp1.FehernheitToCelsius(76));
+            Console.ReadKey();
+        }
+    }
+}
