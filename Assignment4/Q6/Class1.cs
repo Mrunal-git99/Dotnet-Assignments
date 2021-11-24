b9d82e20d63957b7e8b288c9b66ef23ff8188545
daqw <90310791+daqw@users.noreply.github.com>
2021-11-24 23:21:56 +0530

    Add files via upload

---
+++ Assignment4/Q6/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureCalculatorLibrary
+{
+    public class TemperatureClass
+    {
+        public float FehernheitToCelsius(float Fehrenheit)
+        {
+            return (Fehrenheit - 32) * 5 / 9;
+        }
+    }
+}
