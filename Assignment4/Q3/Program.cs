17533efb8f1c97e1202a4e8f0681930cc0ccb02d
daqw <90310791+daqw@users.noreply.github.com>
2021-11-24 23:15:49 +0530

    Add files via upload

---
+++ Assignment4/Q3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapMethod
+{
+     class Program
+    {
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 46, b = 64;
+            Console.WriteLine("Before swap: {0}, {1}", a, b);
+            Swap<int>(ref a, ref b);
+            Console.WriteLine("After swap: {0}, {1}", a, b);
+            Console.ReadLine();
+
+        }
+    }
+}
