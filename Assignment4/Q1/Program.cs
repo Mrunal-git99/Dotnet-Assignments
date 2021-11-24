c9a9dd2922d1633a4563d0ee15ed9b3badef160b
daqw <90310791+daqw@users.noreply.github.com>
2021-11-24 23:11:42 +0530

    Add files via upload

---
+++ Assignment4/Q1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverideFunction
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string DeptName { get; set; }
+
+        public override string ToString()
+        {
+            return this.EmpId + " " + this.EmpName + " " + this.DeptName;
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee() { EmpId = 1, EmpName = "Mrunal", DeptName = "Computer Science" };
+
+            Console.WriteLine(emp.ToString());
+            Console.ReadLine();
+        }
+    }
+}
